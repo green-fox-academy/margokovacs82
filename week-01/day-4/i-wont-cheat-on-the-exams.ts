856c72bd11932663a2aadfd4f9f1b51c24a66de4
margokovacs82 <margo.kovacs@gmail.com>
2018-02-15 17:42:15 +0100

    examspoen

---
+++ week-01/day-4/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+let k: string = "I won\'t cheat on the exam!";
+
+for(let i: number = 1; i <=100; i++){
+
+
+console.log(k);}
