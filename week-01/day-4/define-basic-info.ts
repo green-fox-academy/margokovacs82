49050d525c764ebe04f7a98c9517769d63af5276
margokovacs82 <margo.kovacs@gmail.com>
2018-02-15 14:03:59 +0100

    basicinfo

---
+++ week-01/day-4/define-basic-info.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let margo: string = "Margo Kovacs";
+let age: number = 35;
+let height: number = 1.61;
+let married: boolean = true;
+
+console.log("My name is " + margo); // prints My name is Margo Kovacs
+console.log("My age is " + age); // prints My age is 35
+console.log("My height is " + height); // prints My height is 1.61
+console.log("Married? " + married); // prints Married? true
