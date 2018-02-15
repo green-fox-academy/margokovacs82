1777ef7330023b77f514edcf7eb858a19906c664
margokovacs82 <margo.kovacs@gmail.com>
2018-02-15 17:32:12 +0100

    condivarimutation

--- week-01/day-4/seconds-in-a-day.ts
+++ week-01/day-4/seconds-in-a-day.ts
@@ -7,7 +7,8 @@ let currentSeconds: number = 42;
 // Write a program that prints the remaining seconds (as an integer) from a
 // day if the current time is represented by these variables
 
-let thewholeday: number = (24 * 60 * 60);
+let thewholeday: number = (24 * 60 * 60); // would print out 86.400
 
-let secondstillnow: number = ((14 * 60 * 60) + (34 * 60) + 42);
-console.log(thewholeday - secondstillnow);
+let secondstillnow: number = ((14 * 60 * 60) + (34 * 60) + 42); // would print out 52.482
+
+console.log(thewholeday - secondstillnow); // prints out 33.918
