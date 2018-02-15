d65fe857c19a41a342df274e5a7126196718b5cd
margokovacs82 <margo.kovacs@gmail.com>
2018-02-15 14:09:06 +0100

    newbasicinfo

--- week-01/day-4/define-basic-info.ts
+++ week-01/day-4/define-basic-info.ts
@@ -9,9 +9,17 @@
 let margo: string = "Margo Kovacs";
 let age: number = 35;
 let height: number = 1.61;
-let married: boolean = true;
 
 console.log("My name is " + margo); // prints My name is Margo Kovacs
 console.log("My age is " + age); // prints My age is 35
 console.log("My height is " + height); // prints My height is 1.61
-console.log("Married? " + married); // prints Married? true
+
+let married: boolean = true;
+
+if (married == true) {
+    console.log('I am married');
+  } else {
+    console.log('I am not married'); 
+  }
+
+
