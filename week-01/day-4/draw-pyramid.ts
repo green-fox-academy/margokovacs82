0edc4a152a3380970719e2c73e9cdccecd76ed4f
margokovacs82 <margo.kovacs@gmail.com>
2018-02-17 12:28:51 +0100

    newpyramid

--- week-01/day-4/draw-pyramid.ts
+++ week-01/day-4/draw-pyramid.ts
@@ -1,6 +1,4 @@
 'use strict';
-
-
 // Write a program that draws a
 // pyramid like this:
 //
@@ -15,45 +13,14 @@ let lineC: number = 4;
 let star: string = "*";
 let space: string = " ";
 
+for(let i:number = 1; i<=lineC; i++){
+   let pyramid = '';
 
-for(let i: number = 1; i <=lineC; i++){
-let printOut: string = '';
-
-    for(let j: number = (lineC - i); j > 0; j-- ){
-    printOut += space
-    }    
-
-    for(let k: number = 1; k <=((2*i) - 1); (k++) ){
-    printOut += star
+        for(let j:number = (lineC-i); j >= 1; j--){
+            pyramid += space;
+        }
+            for(let k:number = 1; k<= ((i*2)-1); k++){
+                pyramid += star;
+            }
+        console.log(pyramid);
     }
-
-    console.log(printOut);
-
-}
-
-
-
-
-   
-
-
- 
-// Write a program that draws a
-// pyramid like this:
-//
-//    *
-//   ***
-//  *****
-// *******
-//
-// The pyramid should have as many lines as lineCount is
-
-let line1: string = "   *   ";
-let line2: string = "  ***  ";
-let line3: string = " ***** ";
-let line4: string = "*******";
-
-console.log(line1)
-console.log(line2)
-console.log(line3)
-console.log(line4)
