66283aa638d6129ea7272e20fdaa7e53519ba540
margokovacs82 <margo.kovacs@gmail.com>
2018-02-17 11:27:42 +0100

    conditionals

--- week-01/day-4/conditional-variable-mutation.ts
+++ week-01/day-4/conditional-variable-mutation.ts
@@ -1,37 +1,36 @@
 'use strict';
+// if a is even increment out by one
 
 let a: number = 24;
 let out: number = 0;
 
-// if a is even increment out by one
+if (a%2 === 0){
+    console.log(out + 1);
 
-if (a % 2 == 0)
-{console.log(out + 1);
- 
-  } else {
-    console.log('nothing');
-  }
+}else {
+  console.log("'A' is not even. Please give an even number.");
+}
 
-
+// if b is between 10 and 20 set out2 to "Sweet!"
+// if less than 10 set out2 to "Less!",
+// if more than 20 set out2 to "More!"
 
 let b: number = 13;
 let out2: string = '';
 
-if (b > 10 && b < 20) {
-        out2 = "Sweet!";
-    }else if(b < 10){
-        out2 = "Less!";
-    }else {
-        out2 = "More!";
-    }
-// if b is between 10 and 20 set out2 to "Sweet!"
-// if less than 10 set out2 to "Less!",
-// if more than 20 set out2 to "More!"
-console.log(out2);
+if ((b > 10) && (b < 20)){
+  out2 = "Sweet!";
 
-let c: number = 123;
-let credits: number = 100;
-let isBonus: boolean = false;
+  }else if (b < 10){
+    out2 = "Less!";
+
+  }
+    else {
+      out2 = "More!";
+    }
+
+    console.log(out2);
+
 
 // if credits are at least 50,
 // and is_bonus is false decrement c by 2
@@ -39,21 +38,17 @@ let isBonus: boolean = false;
 // and is_bonus is false decrement c by 1
 // if is_bonus is true c should remain the same
 
-if (credits >= 50 && isBonus === false) {
-    c = c - 2; 
+let c: number = 123;
+let credits: number = 100;
+let isBonus: boolean = false;
 
-  } else if (credits < 50 && isBonus === false) {
-    c--; 
- 
-} else if (isBonus) {
-    c = c;
-  }
-
+if ((credits >= 50) && (isBonus===false)){
+  console.log(c-2);
+} else if ((credits < 50) && (isBonus==false)){
+  console.log(c+1);
+} else if (isBonus!=false) {
   console.log(c);
-
-let d: number =  8;
-let time: number = 120;
-let out3: string = '';
+}
 
 // if d is dividable by 4
 // and time is not more than 200
@@ -62,13 +57,16 @@ let out3: string = '';
 // set out3 to "Time out"
 // otherwise set out3 to "Run Forest Run!"
 
-if ((d % 4 == 0) && (time <= 200)) {
-    out3 = "check"; 
+let d: number =  8;
+let time: number = 120;
+let out3: string = '';
 
-  } else if (time > 200) {
-    out3 = "Time out";
- 
-} else {
-    out3 = "Run Forest Run";
-  }
-  console.log(out3);
+if ((d%4 === 0) && (time <= 200)){
+  out3="check";
+
+  }else if (time > 200){
+    out3="Time out";
+  
+  }else {out3 = "Run forest, run!"} 
+
+console.log(out3);
