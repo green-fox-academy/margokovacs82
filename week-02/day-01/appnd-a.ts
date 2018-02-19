10edae5a08baa660dfa2cf9858383208b3dfc583
margokovacs82 <margo.kovacs@gmail.com>
2018-02-19 17:05:33 +0100

    kutya

---
+++ week-02/day-01/appnd-a.ts
@@ -0,0 +1,10 @@
+// -  Create an array variable named `nimals`
+//    with the following content: `["kuty", "macsk", "cic"]`
+// -  Add all elements an `"a"` at the end
+
+'use strict';
+
+let nimals: string [] = ["kuty", "macsk", "cic"];
+
+nimals = nimals.map(nimals => nimals + "a ");
+console.log(nimals);
--- week-02/day-01/incremet-elements.ts
+++ week-02/day-01/incremet-elements.ts
@@ -1,6 +1,7 @@
 // -  Create a variable named `numList` with the following content: `[1, 2, 3, 4, 5]`
 // -  Increment the third element simply by accessing it
 // -  Log the third element to the console
+'use strict';
 
 let numerList: number [] = [1, 2, 3, 4, 5];
 numerList[2] = numerList[2] + 1;
