0ecca272f42377811ba040afe16816ba977ed34a
margokovacs82 <margo.kovacs@gmail.com>
2018-02-19 10:12:30 +0100

    average

---
+++ week-02/day-01/parametric-average.ts
@@ -0,0 +1,19 @@
+// Write a program that asks for a number.
+// It would ask this many times to enter an integer,
+// if all the integers are entered, it should print the sum and average of these
+// integers like:
+//
+// Sum: 22, Average: 4.4
+//
+// For typescript because we don't have user input (now):
+// Write a program that should print the sum and average of the variables
+// The result like:
+// Sum: 22, Average: 4.4
+
+'use strict';
+
+let a:number = 10;
+let b:number = 7;
+
+console.log(a+b);
+console.log((a+b)/2);
