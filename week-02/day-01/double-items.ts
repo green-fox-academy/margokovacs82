b0d670a1f59c14c1a4d19fdfa5f8e0d620544e25
margokovacs82 <margo.kovacs@gmail.com>
2018-02-19 17:18:32 +0100

    double

---
+++ week-02/day-01/double-items.ts
@@ -0,0 +1,8 @@
+// -  Create an array variable named `names` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
+// -  Double all the strings in the array, use a built in array method instead of a loop
+// It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+
+let names: string [] = ['Gin', 'Whiskey', 'Wine', 'Beer'];
+
+names = names.map(names => names + names);
+console.log(names);
