75a1b719c62e0e329273f098a2c42de3766e3ea4
margokovacs82 <margo.kovacs@gmail.com>
2018-02-20 11:53:34 +0100

    saturn

---
+++ week-02/day-02/simple-replace.ts
@@ -0,0 +1,11 @@
+'use strict';
+// I would like to replace "dishwasher" with "galaxy" in this example
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
+//var re = /apples/gi; 
+
+let re = /dishwasher/gi; 
+let example: string = 'In a dishwasher far far away';;
+let newstr = example.replace(re, "galaxy"); 
+console.log(newstr);
+export = example;
