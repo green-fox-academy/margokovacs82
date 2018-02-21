d7df1571578ac86b846a451bc29a705afd195041
margokovacs82 <margo.kovacs@gmail.com>
2018-02-21 11:07:19 +0100

    diagonal

---
+++ week-02/day-03/diagonals-ts.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// draw a box that has different colored lines on each edge.
+
+ctx.strokeStyle = "green";
+ctx.beginPath();
+ctx.moveTo(10, 10);
+ctx.lineTo(400, 10);
+ctx.stroke();
+
+ctx.strokeStyle = "red";
+ctx.beginPath();
+ctx.moveTo(400, 10);
+ctx.lineTo(400, 210);
+ctx.stroke();
+
+ctx.strokeStyle = "blue";
+ctx.beginPath();
+ctx.moveTo(400, 210);
+ctx.lineTo(10, 210);
+ctx.stroke();
+
+ctx.strokeStyle = "black";
+ctx.beginPath();
+ctx.moveTo(10, 210);
+ctx.lineTo(10, 10);
+ctx.stroke();
+
+
+ctx.strokeStyle = "purple";
+ctx.beginPath();
+ctx.moveTo(10, 10);
+ctx.lineTo(400, 210);
+ctx.stroke();
+
+ctx.strokeStyle = "pink";
+ctx.beginPath();
+ctx.moveTo(400, 10);
+ctx.lineTo(10, 210);
+ctx.stroke();
