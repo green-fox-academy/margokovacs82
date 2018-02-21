3d98a2cf0adcad11f0163d1169a4aeb37a0380b1
margokovacs82 <margo.kovacs@gmail.com>
2018-02-21 14:13:47 +0100

    foursq

--- week-02/day-03/four-rectangles-ts.ts
+++ week-02/day-03/four-rectangles-ts.ts
@@ -11,7 +11,8 @@ let sqSize = 20;
 let sqPos = 0;
 
 for (let i:number = 1; i <= 4; i++){
-    sqPos += 20;
+    sqPos += sqSize + 10;
+    sqSize += 20;
     ctx.fillRect(sqPos, sqPos, sqSize, sqSize);
     ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
 
