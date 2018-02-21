22778cbbc312f486f5ed087e52bccd92a54ecaf1
margokovacs82 <margo.kovacs@gmail.com>
2018-02-21 14:11:35 +0100

    thefour

---
+++ week-02/day-03/four-rectangles-ts.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Draw four different size and color rectangles.
+// Avoid code duplication.
+
+let sqSize = 20;
+let sqPos = 0;
+
+for (let i:number = 1; i <= 4; i++){
+    sqPos += 20;
+    ctx.fillRect(sqPos, sqPos, sqSize, sqSize);
+    ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+
+}
+           
