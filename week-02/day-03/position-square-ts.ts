77ad5710ae96d1316bcbdd361ff2babbee6e88a8
margokovacs82 <margo.kovacs@gmail.com>
2018-02-21 14:37:23 +0100

    posicorrected

--- week-02/day-03/position-square-ts.ts
+++ week-02/day-03/position-square-ts.ts
@@ -9,3 +9,29 @@ const ctx = canvas.getContext('2d');
 // and draws a 50x50 square from that point.
 // Draw 3 squares with that function.
 // Avoid code duplication.
+
+
+//megoldás
+
+function sqToCoords (x: number, y: number){
+    ctx.fillRect(x, y, 50, 50);
+    ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+}
+sqToCoords(10, 30);
+sqToCoords(60, 100);
+sqToCoords(500, 200);
+
+
+//agymenés, de saját
+let squareSizee = 50;
+let x: number = 10;
+let y: number = 30;
+
+function sqLocation(x: number, y: number) {
+    ctx.fillRect(x, y, squareSizee, squareSizee);
+    ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+}
+for (let i:number = 1; i <= 3; i++){
+    sqLocation(x, y);
+    y = y + 60;
+} 
