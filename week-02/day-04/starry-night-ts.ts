7e772901dc3a3a2051469dbf26c6815f88c7a48a
margokovacs82 <margo.kovacs@gmail.com>
2018-02-22 16:19:17 +0100

    stars

--- week-02/day-04/starry-night-ts.ts
+++ week-02/day-04/starry-night-ts.ts
@@ -4,28 +4,32 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
+//  - The stars should be small squares
+//  - The stars should have random positions on the canvas
+//  - The stars should have random color (some shade of grey)
+
 // Draw the night sky:
 //  - The background should be black
+
 ctx.fillStyle = 'black';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
-  let positx: number[] = []
-  let posity: number[] = []
-function stars (positx: number[], posity: number[]) {
-    let amountOfStars: number = 55;
-for (let i = 0; i < amountOfStars; i++) {
 
-    positx[i] = (Math.random() * canvas.width), (Math.random() * canvas.height);
-    posity[i] = (Math.random() * canvas.width), (Math.random() * canvas.height);
-        
-    
-    ctx.fillRect(positx[i], posity[i], 10, 10);
-    ctx.fillStyle = 'rgba(255, 255, 255, 1/(i)';
-    //ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+let positxx: number[] = [];
+let posityy: number[] = [];
+let amountOfStars: number = 55;
+
+function stars (positx: number, posity: number) {
+    ctx.fillRect(positx, posity, 10, 10);
+    //ctx.fillStyle = 'rgba(255, 255, 255, 1/(i)';
+    ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
     //ctx.fillStyle = "rgb("+ Math.floor(Math.random() * 255) + ","+ Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";  
-//console.log(positx, posity);
-}
+    //console.log(positx, posity);
 } 
-stars(positx, posity);  
 
+for (let i = 0; i < amountOfStars; i++) {
+    positxx[i] = (Math.random() * canvas.width), (Math.random() * canvas.height);
+    posityy[i] = (Math.random() * canvas.width), (Math.random() * canvas.height);
+    stars(positxx[i], posityy[i]);  
+}
 
 
