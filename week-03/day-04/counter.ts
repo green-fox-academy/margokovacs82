1ce81489ffacccf1ea3d720764aae6c5c9b21022
margokovacs82 <margo.kovacs@gmail.com>
2018-03-01 09:04:21 +0100

    counter

---
+++ week-03/day-04/counter.ts
@@ -0,0 +1,9 @@
+// Write a recursive function that takes one parameter: n and counts down from n.
+'use strict';
+function factorialN(n: number) {
+  if (n === 1) {return 1;
+  } else {
+    return n * factorialN(n - 1);
+    }
+  }
+console.log('5! is', factorialN(5));
