0468b6553c1eb78c315983e09edea7b155eed832
margokovacs82 <margo.kovacs@gmail.com>
2018-03-01 14:37:07 +0100

    aminal

---
+++ week-03/day-03/animal.ts
@@ -0,0 +1,32 @@
+//Create an Animal class
+//Every animal has a hunger value, which is a whole number
+//Every animal has a thirst value, which is a whole number
+//when creating a new animal object these values are created with the default 50 value
+//Every animal can eat() which decreases their hunger by one
+//Every animal can drink() which decreases their thirst by one
+//Every animal can play() which increases both by one
+'use strict';
+
+class Animals {
+  hunger: number;
+  thirst: number;
+  constructor() {
+    this.hunger = 50;
+    this.thirst = 50;
+  }
+  play() {
+    this.hunger++;
+    this.thirst++;
+  }
+  eat() {
+    this.hunger--;
+  }
+  drink() {
+    this.thirst--;
+  }
+}
+let cat = new Animals();
+while (cat.hunger !== 100) {
+cat.play();
+}
+console.log(cat.hunger);
