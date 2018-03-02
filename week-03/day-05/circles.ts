9db7cf61c2543bfc4d47c3993782bf3357c8cb06
margokovacs82 <margo.kovacs@gmail.com>
2018-03-02 13:29:33 +0100

    draw

---
+++ week-03/day-05/circles.ts
@@ -0,0 +1,25 @@
+'use strict';
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+let sizeX: number = canvas.width / 2;
+let sizeY: number = canvas.height / 2;
+let diagonal: number = canvas.width / 2;
+let repeat: number = 7;
+
+function drawCircle (x: number, y: number, dia: number, rep: number) { 
+  ctx.beginPath();
+  ctx.arc(x, y, dia, 0, Math.PI * 2);
+  ctx.stroke();
+  ctx.strokeStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+  if (rep >= 1) {
+    setTimeout(function(){ 
+    drawCircle(x + dia * Math.sqrt(3)/4, y + dia/4, dia/2, rep - 1);
+    drawCircle(x, y - dia/2, dia/2, rep - 1);
+    drawCircle(x - dia * Math.sqrt(3)/4, y + dia/4, dia/2, rep - 1);
+    }, 500)
+  }
+}
+
+drawCircle(sizeX, sizeY, diagonal, repeat);
+
---
+++ week-03/day-05/triangles.ts
@@ -0,0 +1,38 @@
+'use strict';
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+let xWidth: number = 600;
+let yHeight: number = 500;
+let xCoord: number = 0;
+let yCoord: number = 0;
+let repetition: number = 7;
+
+function drawTriangle(x: number, y: number, width: number, height: number, rep: number) {
+  let widthHalf: number = width / 2;
+  let heithHalf: number = height / 2;
+  let repetition: number = 5;
+
+  ctx.beginPath();
+  ctx.moveTo(x + 0 , y + 0); 
+  ctx.lineTo(x + widthHalf * 2, y + 0);
+  ctx.lineTo(x + widthHalf, y + heithHalf * 2);
+  ctx.lineTo(x + 0, y + 0);
+  ctx.lineTo(x + widthHalf, y + 0);
+  ctx.lineTo(x + widthHalf * 2 - widthHalf/2, y + heithHalf);
+  ctx.lineTo(x + widthHalf/2, y + heithHalf);
+  ctx.lineTo(x + widthHalf, y + 0);
+  ctx.stroke();
+  ctx.strokeStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+     
+      if (rep >= 1) {
+        setTimeout(function(){ 
+          drawTriangle(x + 0, y + 0, widthHalf, heithHalf, rep -1);
+          drawTriangle(x + widthHalf, y + 0, widthHalf, heithHalf, rep -1);
+          drawTriangle(x + widthHalf/2, y + heithHalf, widthHalf, heithHalf, rep -1);
+        }, 1000)
+      }
+    } 
+drawTriangle(xCoord, yCoord, xWidth, yHeight, repetition);
+    
+    
