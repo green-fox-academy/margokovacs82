5238ba0edde1428937f53b07e08efc20d92010aa
margokovacs82 <margo.kovacs@gmail.com>
2018-03-13 11:46:03 +0100

    double

--- practice/day-01-practice/double-words.ts
+++ practice/day-01-practice/double-words.ts
@@ -12,8 +12,20 @@ console.log(alcohol)
 
 // -  Double all the strings in the array, use a built in array method instead of a loop
 // It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+// it is without array:
 let names: string[] = ['Gin', 'Whiskey', 'Wine', 'Beer'];
 names.forEach(function (item: string){
   item = item + item
   console.log(item);
 });
+
+//b-version, with array: 
+
+let names1: string[] = ['Gin', 'Whiskey', 'Wine', 'Beer'];
+let pia: string [] = [];
+
+names1.forEach(function (item: string){
+  pia.push(item + item);
+  return pia;
+});
+console.log(pia);
