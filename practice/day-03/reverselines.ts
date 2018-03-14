b496e595f7463ef8182cee99850b292cc6909c7a
margokovacs82 <margo.kovacs@gmail.com>
2018-03-14 20:58:59 +0100

    more

--- practice/day-03/reverselines.ts
+++ practice/day-03/reverselines.ts
@@ -1,6 +1,20 @@
 'use strict';
 const fs = require('fs');
 
-let fileContent = fs.readFileSync('./reverselines.txt', 'utf-8').split('\r\n');
+//simple:
+let fileConten = fs.readFileSync('./reverse.txt', 'utf-8').split('');
+console.log(fileConten.reverse().join(''));
 
-console.log(fileContent.reverse().join('\r\n'));
+// if i want to write it to an other file: 
+let fileContent2 = fs.readFileSync('./reverselines.txt', 'utf-8').split('\r\n');
+let newFile = fileContent2.reverse().join('\r\n');
+fs.writeFileSync('./newlines.txt', newFile);
+
+// with error handling: 
+try{
+let fileContent3 = fs.readFileSync('./reve.txt', 'utf-8').split('\r\n');
+let newFile = fileContent3.reverse().join('\r\n');
+fs.writeFileSync('./newlines.txt', newFile);
+} catch(err) {
+  console.log('sorry')
+}
