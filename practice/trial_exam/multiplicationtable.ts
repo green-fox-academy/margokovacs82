7d12fa63b96c9fc716a034d0b70a314ff47efb3b
margokovacs82 <margo.kovacs@gmail.com>
2018-03-18 15:05:05 +0100

    multi

---
+++ practice/trial_exam/multiplicationtable.ts
@@ -0,0 +1,23 @@
+'use strict';
+// Create a function that prints the multiplication table of a given base number.
+// It should take the base number as a parameter and prints the output to the
+// console.
+
+function multi(base: number) {
+  let result: number = 0;
+  for (let j: number = 1; j <= 10; j++) {
+    console.log(j + ' * ' + base + ' = ' + (result = j * base));  
+  } 
+}
+console.log(multi(5));
+// The output should look like this for 5 as base:
+//  1 * 5 = 5
+//  2 * 5 = 10
+//  3 * 5 = 15
+//  4 * 5 = 20
+//  5 * 5 = 25
+//  6 * 5 = 30
+//  7 * 5 = 35
+//  8 * 5 = 40
+//  9 * 5 = 45
+//  10 * 5 = 50
