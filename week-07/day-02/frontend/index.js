063159ab19dbead864d8428f345afaea6bb54528
margokovacs82 <margo.kovacs@gmail.com>
2018-04-03 17:37:42 +0200

    yoda

--- week-07/day-02/frontend/index.js
+++ week-07/day-02/frontend/index.js
@@ -25,16 +25,16 @@ app.listen(PORT, () => {
 app.get('/doubling', (req, res) => {
   const input = req.query.input;
   console.log(input);
-    if (input === undefined) {
-      res.json({
-        error: 'Please provide an input!'
-      })
-    } else {
-        res.json ({
-          "received": input,
-          "result": input * 2, 
-        })
-    }
+	if (input === undefined) {
+	  res.json({
+		error: 'Please provide an input!'
+	  })
+	} else {
+		res.json ({
+		  "received": input,
+		  "result": input * 2, 
+		})
+	}
 });
 
 
@@ -43,19 +43,19 @@ app.get('/greeter', (req, res) => {
   const name = req.query.name;
   const title = req.query.title;
   if (name === undefined) {
-    res.json({
-      error: 'Please provide a name!'
-    })
+	  res.json({
+	    error: 'Please provide a name!'
+	  })
   }
   else if (title === undefined) {  
-    res.json({
-      error: 'Please provide a title!'
-    })
+	  res.json({
+	    error: 'Please provide a title!'
+	  })
   } else {
-    res.json ({
-    //name=Petike&title=student
-      "welcome_message": `Oh, hi there ${name}, my dear ${title}!`
-    })
+	res.json ({
+	//name=Petike&title=student
+	  "welcome_message": `Oh, hi there ${name}, my dear ${title}!`
+	})
   }
 });
 
@@ -65,13 +65,13 @@ app.get('/appenda/:appendable', (req, res) => {
   const appendable = req.params.appendable;
   console.log(appendable);
   if (appendable === undefined) {
-    res.status(404);
-    res.end();
+	  res.status(404);
+	  res.end();
   } else {
-      res.json ({
-        appended: appendable + 'a', 
-      })
-    }
+	  res.json ({
+		appended: appendable + 'a', 
+	  })
+	}
 });
 
 
@@ -79,19 +79,19 @@ app.get('/appenda/:appendable', (req, res) => {
 app.post('/dountil/:thing', function(req, res) {
   const thing = req.params.thing;
   let numb = 1;
-    if (thing === 'sum') {
-      for (let i=2; i<req.body.until + 1; i++ ) {
-        numb = numb + i;
-      }
-    } 
-    if (thing === 'factor') {
-      for (let i=2; i<req.body.until + 1; i++ ) {
-        numb *= i;
-      }
-    } 
-    res.json({
-      "result": numb
-    });
+	if (thing === 'sum') {
+	  for (let i=2; i<req.body.until + 1; i++ ) {
+	  	numb = numb + i;
+	  }
+	} 
+	if (thing === 'factor') {
+	  for (let i=2; i<req.body.until + 1; i++ ) {
+		  numb *= i;
+	  }
+	} 
+	res.json({
+	  "result": numb
+	});
 });
 
 
@@ -99,54 +99,54 @@ app.post('/dountil/:thing', function(req, res) {
 app.post('/arrays/', function(req, res) {
   const what = req.body.what;
   if (what === undefined) {
-    res.json({
-      error: "Please provide what to do with the numbers!"
-    })
+	  res.json({
+	    error: "Please provide what to do with the numbers!"
+	  })
   }
   else if (what === 'sum') {
-    result = 0;
-    for (let i=0; i<req.body.numbers.length; i++ ) {
-      result += req.body.numbers[i];
-    }
+  	result = 0;
+  	for (let i=0; i<req.body.numbers.length; i++ ) {
+	    result += req.body.numbers[i];
+  	}
   } 
   else if (what === 'multiply') {
-    result = 1;
-    for (let i=0; i<req.body.numbers.length; i++ ) {
-      result *= req.body.numbers[i];
-    }
+  	result = 1;
+  	for (let i=0; i<req.body.numbers.length; i++ ) {
+	    result *= req.body.numbers[i];
+	  }
   } 
   else if (what === 'double') {
-    result = []
-    for (let i=0; i<req.body.numbers.length; i++ ) {
-      result.push(req.body.numbers[i] * 2);
-    }
+	result = []
+	  for (let i=0; i<req.body.numbers.length; i++ ) {
+	    result.push(req.body.numbers[i] * 2);
+	  }
   } 
   res.json({
-    "result": result
+	"result": result
   });
 });
 
 //The Reverser of the Sith
 app.post('/yoda/', function(req, res) {
-    const text = `${req.body.text} `;
-    sentences = text.split('. ');
-    let str = [];
+	const text = `${req.body.text} `;
+	sentences = text.split('. ');
+	let str = [];
    
-    for (let i=0; i < sentences.length - 1; i++) {
-        str.push(sentences[i].split(' '));
-    }
-    
-    let newStr = [];
-    for (let i=0; i < str.length; i++) {
-      for (let j=0; j<str[i].length; j+=2) {
-          if (str[i][j+1] !== undefined) {
-            newStr.push(str[i][j+1]);
-          }
-          newStr.push(str[i][j]);
-      }
-		} 
+	for (let i=0; i < sentences.length - 1; i++) {
+		str.push(sentences[i].split(' '));
+	}
+	
+	let newStr = [];
+	for (let i=0; i < str.length; i++) {
+	  for (let j=0; j<str[i].length; j+=2) {
+		  if (str[i][j+1] !== undefined) {
+			newStr.push(str[i][j+1]);
+		  }
+		    newStr.push(str[i][j]);
+	  }
+	} 
 		
-    res.json({
-        "result": newStr.join(' ')
-    });
+	res.json({
+		"result": newStr.join(' ')
+	});
 });
