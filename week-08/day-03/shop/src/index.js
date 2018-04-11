f0d98412e51717d9d6ee70ca3f7e2bc85c3a0b63
margokovacs82 <margo.kovacs@gmail.com>
2018-04-11 20:32:53 +0200

    almostdone

--- week-08/day-03/shop/src/index.js
+++ week-08/day-03/shop/src/index.js
@@ -14,10 +14,25 @@ const conn = mysql.createConnection({
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
+const types = {
+  entertainment: 1,
+  groceries: 2,
+  restaurant: 3,
+  services: 4,
+  transport: 5,
+};
 
 app.get('/', (req, res) => {
-  const sql = `SELECT * FROM history ORDER BY date DESC LIMIT 15;`;
-  conn.query(sql, (err, result) => {
+  let sql = `SELECT * FROM history ORDER BY date DESC LIMIT 15;`;
+  let queryInputs = [];
+  if (req.query.type !== undefined) {
+    sql = `SELECT * FROM history WHERE type = ? LIMIT 25;`;
+    queryInputs = [types[req.query.type]];
+  }
+
+  conn.query(sql, queryInputs, (err, result) => {
     if (err) {
       console.log(err);
       res.sendStatus(500);
@@ -29,7 +44,34 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/api/history', (req, res) => {
+  let sql = `SELECT * FROM history ORDER BY date DESC LIMIT 15;`;
+  let queryInputs = [];
+  if (req.query.type !== undefined) {
+    sql = `SELECT * FROM history WHERE type = ? LIMIT 25;`;
+    queryInputs = [types[req.query.type]];
+  }
 
+  conn.query(sql, queryInputs, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+    res.json(result);
+  });
+});
+
+app.get('/api/types', (req, res) => {
+  let sql = `SELECT * FROM types;`;
+ 
+  conn.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+    res.json(result);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`App is running on ${PORT}`);
