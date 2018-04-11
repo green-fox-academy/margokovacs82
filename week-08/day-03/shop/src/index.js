f247a57b55cfd0c7c572080614ec21b7a5d8f3e9
margokovacs82 <margo.kovacs@gmail.com>
2018-04-11 19:36:07 +0200

    newfiles

--- week-08/day-03/shop/src/index.js
+++ week-08/day-03/shop/src/index.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql');
 
 const app = express();
 const PORT = 3000;
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'x',
+  database: 'shop',
+  port: 3306,
+})
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Dashboard',
+  const sql = `SELECT * FROM history ORDER BY date DESC LIMIT 15;`;
+  conn.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+    res.render('index', {
+      title: 'Dashboard',
+      history: result,
+    });
   });
 });
 
