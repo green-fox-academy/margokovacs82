d158712d83a7ccf8d1b5688eb254f3185aa89928
margokovacs82 <margo.kovacs@gmail.com>
2018-04-12 10:18:38 +0200

    reddit backend done

--- week-07/day-05/reddit/index.js
+++ week-07/day-05/reddit/index.js
@@ -5,6 +5,8 @@ let mysql = require('mysql');
 let moment = require('moment');
 
 app.use(express.json());
+//app.use('/static', express.static('static'));
+//app.set('view engine', 'ejs');
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
@@ -21,6 +23,7 @@ var connection = mysql.createConnection({
   database: 'reddit',
 });
 
+//show a post
 app.get('/posts', (req, res) => {
   connection.query('SELECT * FROM posts', function (error, results, fields) {
     if (error) throw error;
@@ -28,13 +31,14 @@ app.get('/posts', (req, res) => {
   });
 });
 
-app.post('/posts', function(req, res){
+//make a new record
+app.post('/posts', function(req, res) {
   timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
   title = req.body.title;
   url = req.body.url;
  
   connection.query("INSERT INTO posts (timestamp, title, url, score, owner) VALUES (?, ?, ?, ?, ?)", 
-  [timestamp, title, url, 0, 'sanyi'],
+  [timestamp, title, url, 0, 'lali'],
   function(err, result){
     if(err) {
       console.log(err);
@@ -45,4 +49,66 @@ app.post('/posts', function(req, res){
     }
   });
 });
+
+//upvote and downvote
+app.put('/posts/:id/upvote', function(req, res) {
+  
+  let upvote = `UPDATE posts SET score = score + 1 WHERE id = ${req.params.id};`;
+  connection.query(upvote, (err, result) => {
+    if(err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      console.log("1 record updated");
+      res.json(result);
+    }
+  });
+});
+
+app.put('/posts/:id/downvote', function(req, res) {
+  
+  let downvote = `UPDATE posts SET score = score - 1 WHERE id = ${req.params.id};`;
+  connection.query(downvote, (err, result) => {
+    if(err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      console.log("1 record updated");
+      res.json(result);
+    }
+  });
+});
+
+//modify a record - in progress
+app.put('/posts/:id', function(req, res) {
+  
+    let modifications = `UPDATE posts SET title = '${req.body.title}', url = '${req.body.url}' WHERE id = ${req.params.id};`; 
+    connection.query(modifications, (err, result) => {
+      if(err) {
+        console.log(err);
+        res.sendStatus(500);
+      } else {
+        console.log("1 record updated");
+        res.json(result);
+      }
+    });
+  });
+  
+
+//delete a record
+app.delete('/posts/:id', function(req, res) {
+
+  let deleteQuery = `DELETE FROM posts WHERE id = ${req.params.id};`;
+  connection.query(deleteQuery, (err, result) => {
+    if(err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      console.log("1 record deleted");
+      res.json(result);
+    }
+  });
+});
+
+
 
