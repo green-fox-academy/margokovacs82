eb6c5e0211b0a46bdb15993f447d3a41245eb2d2
margokovacs82 <margo.kovacs@gmail.com>
2018-04-12 10:36:10 +0200

    shopdone

--- week-08/day-03/shop/src/index.js
+++ week-08/day-03/shop/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mysql = require('mysql');
+const moment = require('moment');
 
 const app = express();
 const PORT = 3000;
@@ -15,6 +16,7 @@ const conn = mysql.createConnection({
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use(express.json());
 
 const types = {
   entertainment: 1,
@@ -40,6 +42,7 @@ app.get('/', (req, res) => {
     res.render('index', {
       title: 'Dashboard',
       history: result,
+      moment,
     });
   });
 });
@@ -70,6 +73,21 @@ app.get('/api/types', (req, res) => {
       res.sendStatus(500);
     }
     res.json(result);
+  });
+});
+
+app.post('/api/history', (req, res) => {
+  const sql = 'INSTERT INTO history (title, price, type, date) VALUES (?, ?, ?, ?);';
+  const queryInputs = [req.body.title, req.body.price, req.body.type, req.body.date];
+  conn.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+    res.json({
+      message: 'item has been created',
+      id: result.insertId,
+    });
   });
 });
 
